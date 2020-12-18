9cd269e1b0cf9eb2af849f253086b0027a370970
Benedikt Driessen <benedikt.driessen@hensoldt-cyber.de>
2020-12-18 13:03:20 +0100

    [NEW] SEOS-1936: Add ContextMgr lib

---
+++ include/lib_server/ContextMgr.h
@@ -0,0 +1,113 @@
+/*
+ * Copyright (C) 2020, Hensoldt Cyber GmbH
+ */
+
+/**
+ * @file
+ * @brief The ContextMgr manages client contexts of RPC server components
+ */
+
+#pragma once
+
+#include "OS_Error.h"
+
+#include <stdint.h>
+#include <stddef.h>
+#include <stdbool.h>
+
+// Forward declaration
+typedef struct ContextMgr_ClientSlot ContextMgr_ClientSlot_t;
+
+/**
+ * Type of a seL4 client ID
+ */
+typedef uint32_t ContextMgr_CID_t;
+
+/**
+ * These functions will be called by the ContextMgr to alloc/free a client
+ * context. Note that client contexts will be allocated the first time a
+ * get() is performed, and not before.
+ */
+typedef struct
+{
+    OS_Error_t (*init)(
+        const ContextMgr_CID_t cid, void** mem);
+    OS_Error_t (*free)(
+        const ContextMgr_CID_t cid, void* mem);
+} ContextMgr_MemoryFuncs_t;
+
+/**
+ * Context of context manager, needs to be allocated by user of ContextMgr.
+ */
+typedef struct
+{
+    size_t max;
+    ContextMgr_ClientSlot_t* slots;
+    ContextMgr_MemoryFuncs_t memFns;
+} ContextMgr_t;
+
+/**
+ * @brief Initialize a context manager instance
+ *
+ * Initialize a context manager instance with memory alloc/free callbacks and the
+ * max number of expected contexts. Will internally alloc as many slots for
+ * contexts as indicated by \p max.
+ *
+ * @return an error code
+ * @retval OS_SUCCESS if operation succeeded
+ * @retval OS_ERROR_INVALID_PARAMETER if a parameter was missing or invalid
+ * @retval OS_ERROR_INSUFFICIENT_SPACE if the required amount of handles results
+ *  in a greater need of memory (than the one passed).
+ */
+OS_Error_t
+ContextMgr_init(
+    ContextMgr_t*                   self,   /**< [in]   pointer to context
+                                                        manager */
+    const ContextMgr_MemoryFuncs_t* memFns, /**< [in]   client context alloc/free
+                                                        callbacks */
+    const size_t                    max     /**< [in]   maximum amount of contexts
+                                                        expected */
+);
+
+/**
+ * @brief Free a context manager instance
+ *
+ * Free context manager memory; will call the free() callback on all already
+ * allocated client memories.
+ *
+ * In a typical scenario (e.g., in a component) this will probably never be
+ * called, but it is still good practice to have it.
+ *
+ * @return an error code
+ * @retval OS_SUCCESS if operation succeeded
+ * @retval OS_ERROR_INVALID_PARAMETER if a parameter was missing or invalid
+ */
+OS_Error_t
+ContextMgr_free(
+    ContextMgr_t* self  /**< [in] pointer to context manager */
+);
+
+/**
+ * @brief Get a client context based on its ID
+ *
+ * This function obtains a pointer to a client memory context, based a unique
+ * client ID (CID) parameter.
+ *
+ * If there is currently no memory allocated for that CID, the context manager
+ * will use the init() callback to allocate a client context; then the newly
+ * allocated memory will be returned.
+ *
+ * @return an error code
+ * @retval OS_SUCCESS if operation succeeded
+ * @retval OS_ERROR_INVALID_PARAMETER if a parameter was missing or invalid
+ * @retval OS_ERROR_INSUFFICIENT_SPACE if there is no slot assigned to the
+ *  \p CID but there are no more free slots left.
+ */
+OS_Error_t
+ContextMgr_get(
+    ContextMgr_t*          self,    /**< [in]   pointer to context manager */
+    const ContextMgr_CID_t cid,     /**< [in]   unique client ID to use for
+                                                lookup */
+    void**                 ctx      /**< [out]  pointer which will be set to client
+                                                context mem */
+);
---
+++ src/ContextMgr.c
@@ -0,0 +1,142 @@
+/*
+ * Copyright (C) 2020, Hensoldt Cyber GmbH
+ */
+
+#include "lib_server/ContextMgr.h"
+#include "lib_macros/Check.h"
+#include "lib_util/PointerVector.h"
+
+#include <string.h>
+
+// These are pretty arbitrary, so we can provide between 1-1024 contexts; this
+// can be changed without side effects (given that the memory is there..)
+#define CONTEXTMGR_CONTEXTS_MIN 1
+#define CONTEXTMGR_CONTEXTS_MAX 1024
+
+// Client memory container
+struct ContextMgr_ClientSlot
+{
+    bool inUse;
+    ContextMgr_CID_t cid;
+    void* mem;
+};
+
+// Public functions ------------------------------------------------------------
+
+OS_Error_t
+ContextMgr_init(
+    ContextMgr_t*                   self,
+    const ContextMgr_MemoryFuncs_t* memFns,
+    const size_t                    max)
+{
+    CHECK_PTR_NOT_NULL(self);
+    CHECK_PTR_NOT_NULL(memFns);
+    CHECK_PTR_NOT_NULL(memFns->init);
+    CHECK_PTR_NOT_NULL(memFns->free);
+    CHECK_VALUE_IN_CLOSED_INTERVAL(max,
+                                   CONTEXTMGR_CONTEXTS_MIN,
+                                   CONTEXTMGR_CONTEXTS_MAX);
+
+    self->memFns = *memFns;
+    self->max    = max;
+
+    // Allocate as many client slots as user requested
+    if ((self->slots = calloc(max, sizeof(ContextMgr_ClientSlot_t))) == NULL)
+    {
+        Debug_LOG_ERROR("calloc() failed");
+        return OS_ERROR_INSUFFICIENT_SPACE;
+    }
+
+    return OS_SUCCESS;
+}
+
+OS_Error_t
+ContextMgr_free(
+    ContextMgr_t* self)
+{
+    OS_Error_t err;
+    ContextMgr_ClientSlot_t* slot;
+
+    CHECK_PTR_NOT_NULL(self);
+
+    // Go through client array and call respective free on those which were
+    // allocated
+    for (size_t i = 0; i < self->max; i++)
+    {
+        slot = &self->slots[i];
+        if (slot->mem != NULL)
+        {
+            if ((err = self->memFns.free(slot->cid, slot->mem)) != OS_SUCCESS)
+            {
+                Debug_LOG_ERROR("free() callback failed on client (CID=%i) " \
+                                "with %d, continuing", slot->cid, err);
+            }
+            slot->mem = NULL;
+            slot->cid = 0;
+            slot->inUse = false;
+        }
+    }
+
+    free(self->slots);
+
+    return OS_SUCCESS;
+}
+
+OS_Error_t
+ContextMgr_get(
+    ContextMgr_t*          self,
+    const ContextMgr_CID_t cid,
+    void**                 ctx)
+{
+#define INVALID_SLOT ((size_t) -1)
+    OS_Error_t err;
+    ContextMgr_ClientSlot_t* slot;
+    size_t freeSlot;
+
+    CHECK_PTR_NOT_NULL(self);
+    CHECK_PTR_NOT_NULL(ctx);
+
+    // Check if we already have a slot for this CID
+    freeSlot = INVALID_SLOT;
+    for (size_t i = 0; i < self->max; i++)
+    {
+        slot = &self->slots[i];
+        if (slot->inUse)
+        {
+            if (slot->cid == cid)
+            {
+                Debug_ASSERT_PRINTFLN(slot->mem != NULL,
+                                      "Client (CID=%i) context mem is NULL", cid);
+                *ctx = slot->mem;
+                return OS_SUCCESS;
+            }
+        }
+        else
+        {
+            freeSlot = i;
+        }
+    }
+
+    // CID was not assigned yet, so hopefully we have a free slot
+    if (INVALID_SLOT != freeSlot)
+    {
+        slot = &self->slots[freeSlot];
+        Debug_ASSERT_PRINTFLN(slot->mem == NULL,
+                              "Client memory slot %Zd is unused, but not " \
+                              "NULL", freeSlot);
+        slot->cid = cid;
+        slot->inUse = true;
+        if ((err = self->memFns.init(cid, &slot->mem)) != OS_SUCCESS)
+        {
+            Debug_LOG_ERROR("init() callback failed on client (CID=%i) " \
+                            "with %d", cid, err);
+            return err;
+        }
+        *ctx = slot->mem;
+        return OS_SUCCESS;
+    }
+
+    Debug_LOG_ERROR("Could not find free context slot for client (CID=%i)", cid);
+
+    return OS_ERROR_INSUFFICIENT_SPACE;
+}
